ded7e5531f6153d5c7240735ba53959f79bfa7bd
flaviogdiniz <flaviogdiniz@gmail.com>
2023-09-23 15:05:58 -0300

    Segundo Commit

--- src/TipoVariaveis.java
+++ src/TipoVariaveis.java
@@ -1,5 +1,7 @@
 public class TipoVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Ola!");
+        int idade = 39;
+        String nome = "Flavio";
+        System.out.println("Ola! Meu nome é  " + nome + " e tenho " + idade + " anos");
     }
 }
